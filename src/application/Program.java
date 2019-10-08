652a5160701512f150dfadf094853b3014417390
Marcos Guilherme <marcosfs.guilherme@gmail.com>
2019-10-08 13:40:56 -0300

    Versao com interface

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import model.entities.AbstractShape;
 import model.entities.Circle;
 import model.entities.Rectangle;
 import model.entities.Shape;
@@ -9,7 +10,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Shape s1 = new Circle(2.0,Color.BLACK);
+		AbstractShape s1 = new Circle(2.0,Color.BLACK);
 		Shape s2 = new  Rectangle(3.0,4.0,Color.WHITE);
 		
 		System.out.println(s1);
